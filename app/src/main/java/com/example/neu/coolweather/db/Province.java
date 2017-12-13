522dc7a65b582a2aaec67ad8fdf1917c30627004
Zhaorong Wei <wei.zhr@neusoft.com>
2017-12-13 15:45:28 +0800

    Add database and table configuration.

---
+++ app/src/main/java/com/example/neu/coolweather/db/Province.java
@@ -0,0 +1,41 @@
+package com.example.neu.coolweather.db;
+
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by neu on 17-12-13.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private  int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
